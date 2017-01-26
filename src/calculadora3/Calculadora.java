99c7c9f0fa5762e6285cb9cd16357265c581fc10
piedad soriano <piedad.soriano@gmail.com>
2017-01-26 15:44:25 +0100

    añadimos comprobacion en division

---
+++ src/calculadora3/Calculadora.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculadora3;
+
+/**
+ *
+ * @author piedad
+ */
+public class Calculadora {
+
+public double suma(double number1, double number2){
+
+return number1 + number2;
+
+}
+
+public double resta(double number1, double number2){
+
+return number1 - number2;
+
+}
+
+public double multiplicacion(double number1, double number2) {
+
+return number1 * number2;
+
+}
+
+public double division(double number1, double number2){
+if (number2!=0) 
+    return number1 / number2;
+else return 0;
+
+}
+
+}
